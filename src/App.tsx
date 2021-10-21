e53e5bc0d0896e8d5c9148aade4e29f625291a98
João Mangueira <jamjs7@gmail.com>
2021-10-21 18:54:25 -0300

    jogador se movimentnado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import { useEffect } from 'react';
 import * as Components from './App.styles';
 import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
 
 const App = () => {
+  const char = useCharacter();
+
+  useEffect(() => {
+     window.addEventListener('keydown', handleKeyDown)
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+        switch (e.code) {
+            case 'KeyA':
+            case 'ArrowLeft':
+                char.moveLeft();
+                break;
+            case 'KeyW':
+            case 'ArrowUp':
+                char.moveTop()
+                break;
+            case 'KeyD':
+            case 'ArrowRight':
+                char.moveRight()
+                break;
+            case 'KeyS':
+            case 'ArrowDown':
+                char.moveDown()
+                break;
+
+        }
+  }
+
   return (
       <Components.Container>
         <Components.Map>
-            <Character x={5} y={0}/>
+            <Character x={char.x} y={char.y}/>
         </Components.Map>
       </Components.Container>
   );
