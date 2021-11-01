a2b80c8a3a823c973858f7e79e1f127c1ff949fa
João Mangueira <jamjs7@gmail.com>
2021-11-01 11:12:01 -0300

    Mapa funcionando

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { CharacterSides } from '../types/CharacterSides';
+import { mapSpots } from '../data/mapSpots';
 
 export const useCharacter = () => {
     const [pos, setPos] = useState({ x: 3, y: 5});
     const [side, setSide] = useState<CharacterSides>('down');
 
     const moveLeft = () => {
+
         setPos(pos => ({
-            x: pos.x - 1,
+            x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
             y: pos.y
         }));
 
@@ -16,7 +18,7 @@ export const useCharacter = () => {
 
     const moveRight = () => {
         setPos(pos => ({
-            x: pos.x + 1,
+            x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
             y: pos.y
         }));
 
@@ -26,7 +28,7 @@ export const useCharacter = () => {
     const moveTop = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y - 1
+            y:  canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y
         }));
 
         setSide('up');
@@ -35,10 +37,20 @@ export const useCharacter = () => {
     const moveDown = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y + 1
+            y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y
         }));
 
         setSide('down');
+    }
+
+    const canMove = (x: number, y:number) => {
+
+        if(mapSpots[y] !== undefined && mapSpots[y][x] !== undefined) {
+            if(mapSpots[y][x] === 1) {
+                return true;
+            }
+        }
+        return false;
     }
 
     return {
